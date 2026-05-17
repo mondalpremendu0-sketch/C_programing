9fadeff5554e01057a3c41c5cf15972a8fd673e9
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-17 22:19:34 +0530

    star table done

---
+++ Day_10/Advance_Pattren/star_table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (){
+  int n =4;
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j <= n-i; j++) {
+     printf("*");
+    
+    }
+    for (int j = 1; j <= 2*i; j++) {
+     printf(" ");
+    }
+    for (int j = 1; j <= n-i; j++) {
+     printf("*");
+     
+    }
+    printf("\n");
+  }
+  return 0;
+}

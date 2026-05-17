31317beb301f8bd587d8b3b13a7a5ba37c5183a8
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-17 23:17:35 +0530

    lower portion done and 10 day streak done

--- Day_10/Advance_Pattren/butterfly.c
+++ Day_10/Advance_Pattren/butterfly.c
@@ -14,5 +14,18 @@ int main (){
     }
     printf("\n");
   }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n-i; j++) {
+     printf("*");
+    }
+    for (int j = 1; j <= 2*i; j++) {
+     printf(" ");
+    }
+    for (int j = 1; j <= n-i; j++) {
+     printf("*");
+    }
+    
+    printf("\n");
+  }
   return 0;
 }

ee934db9481b065b84c1e135aa1f566c15b72611
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-18 09:02:35 +0530

    function prototype done

---
+++ Day_11/Function/basic_function/func_prototype.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void India(){
+  printf("We are in India!\n");
+  void Australia();
+  Australia();
+  return;
+}
+int main (){
+   India();
+  return 0;
+}
+void Australia (){
+  printf("We are in Australia!\n");
+   void Indore();
+   Indore();
+  return;
+}
+void Indore(){
+  printf("We are in Indore!\n");
+  return;
+}

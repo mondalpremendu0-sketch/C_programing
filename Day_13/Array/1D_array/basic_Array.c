f5760620d813625c4eb8dd61a5d9437e2167b49a
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-20 17:28:07 +0530

    change the array value

--- Day_13/Array/1D_array/basic_Array.c
+++ Day_13/Array/1D_array/basic_Array.c
@@ -2,11 +2,13 @@
 int main()
 {
   int arr[5] = {1,2,3,4,5};
-  printf("%d ",arr[0]);  
+  printf("%d ",arr[0]);
+  arr[1] = 23;
   printf("%d ",arr[1]);
   printf("%d ",arr[2]);
   printf("%d ",arr[3]);
   printf("%d ",arr[4]);
+  
   return 0;
 }
   

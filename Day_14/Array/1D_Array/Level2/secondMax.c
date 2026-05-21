c2c851faf41fb7b739ab9e6442784c2e6b390695
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-21 10:40:32 +0530

    solved by 3rd method and it pass the all cases

--- Day_14/Array/1D_Array/Level2/secondMax.c
+++ Day_14/Array/1D_Array/Level2/secondMax.c
@@ -41,12 +41,35 @@ int SecondMax_Method_2(int arr[])
   return smax;
 }
 
+int SecondMax_Method_3(int arr[])
+{
+  int max = arr[0];
+  int smax = arr[1];
+  for (int i = 0; i < 5; i++) 
+  {
+    if(max < arr[i])
+    {
+      smax = max;
+      max = arr[i];
+    }
+    
+    else if (arr[i] != max && smax < arr[i])
+    {
+      smax = arr[i];
+    }
+  }
+
+  return smax;
+}
+
 
 
 int main()
 {
-  int arr[5] = {1,2,3,4,5};
+  int arr[5] = {1,6,3,4,5};
   printArray(arr);
+  printf("\n%d",SecondMax_Method_1(arr));
   printf("\n%d",SecondMax_Method_2(arr));
+  printf("\n%d",SecondMax_Method_3(arr));
   return 0;
 }

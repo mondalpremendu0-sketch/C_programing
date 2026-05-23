8ebf173d025cbd988ec9e33be8974ca8155062aa
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-23 11:30:44 +0530

    solved by 2nd method not using extra variables

--- Day_16/Recursion/print_1-n.c
+++ Day_16/Recursion/print_1-n.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-void printNum(int n,int i)
+void printNum_method1(int n,int i)
 {
   if(n == 0) return;
   printf("%d ",i);
-  printNum(n-1,i+1);
+  printNum_method1(n-1,i+1);
 }
+
+void printNum_method2(int n)
+{
+  if(n == 0) return;
+  printNum_method2(n-1);
+  printf("%d ",n);
+}
+
+
 int main (){
   int n ;
   printf("Enter a number: ");
   scanf("%d",&n);
-  printNum(n,1);
-  
+  printNum_method1(n,1);
+  printf("\n");
+  printNum_method2(n);
   return 0;
 }

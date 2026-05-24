305109d9504f0147df94103c9365aae3c4bc1934
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-24 11:03:55 +0530

    same problem solved by 2 different method by optimising it

--- Day_17/Recursion/Level2/a_power_b.c
+++ Day_17/Recursion/Level2/a_power_b.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int result(int a,int b){
+int Method_1(int a,int b){
   if(b == 1) return a;
-  return a * result(a,b-1);
+  return a * Method_1(a,b-1);
 }
+
+int Method_2(int a,int b){
+  if(b == 1) return a;
+  return Method_2(a,b/2) * Method_2(a,b/2);
+}
+
 
 int main (){
   int base,expo;
@@ -11,6 +17,7 @@ int main (){
   scanf("%d",&base);
   printf("Enter exponent: ");
   scanf("%d",&expo);
-  printf("%d to the power %d is = %d\n",base,expo,result(base,expo));
+  printf("%d to the power %d is = %d\n",base,expo,Method_1(base,expo));
+  printf("%d to the power %d is = %d\n",base,expo,Method_2(base,expo));
   return 0;
 }

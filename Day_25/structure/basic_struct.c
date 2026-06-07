0ba59185c140062ee5382f83088ce1c787942f7e
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-07 13:30:38 +0530

    another way of decleartion

--- Day_25/structure/basic_struct.c
+++ Day_25/structure/basic_struct.c
@@ -4,17 +4,21 @@ int main(){
     int hp;
     int attack;
     int speed;
-  };
+  }picachu,charisad;
   
-  struct pokemon picachu;
+  //struct pokemon picachu;
   picachu.hp = 50;
+  printf("Enter picachu hp: ");
+  scanf("%d",&picachu.hp);
   picachu.attack = 70;
   picachu.speed = 100;
   
-  struct pokemon charisad;
-  picachu.hp = 80;
-  picachu.attack = 150;
-  picachu.speed = 70;
+  //struct pokemon charisad;
+  charisad.hp = 80;
+  charisad.attack = 150;
+  charisad.speed = 70;
+  
+  printf("%d\n",picachu.hp);
   
   return 0;
 }

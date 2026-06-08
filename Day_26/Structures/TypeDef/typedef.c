885fc5032514d0dd01202c1d76f2c0a350d94505
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-08 23:04:38 +0530

    learn costum data types like typedef

---
+++ Day_26/Structures/TypeDef/typedef.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+typedef float decimal;
+typedef int Integer;
+typedef char Charecter;
+int main(){
+  decimal Pi = 3.14;
+  printf("%f\n",Pi);
+  
+  Integer x = 500;
+  printf("%d\n",x);
+  
+  Charecter ch = 'a';
+  printf("%c\n",ch);
+  return 0;
+}
